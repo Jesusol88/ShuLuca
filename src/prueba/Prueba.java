e4d8a89962ffc37c833d8087122e70495ce1a80e
admin <admin@tplwkpcente-a9>
2018-06-29 08:33:21 +0200

    cambio en mikel

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,9 +4,9 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Holaquetal");
+System.out.println("Holaque");
 System.out.println("ahora con commit");
-System.out.println("nuevo");
+System.out.println("ahora no es igual");
 System.out.println( suma.sumador(10, 5) );
 System.out.println("antes pull");
 	}
