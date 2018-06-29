915bd20af6b3f0afdb7a8ac515f0d005638e60d6
admin <admin@tplwkpcente-a9>
2018-06-29 08:41:54 +0200

    con error en mikel2

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,11 +4,10 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Holaque");
-System.out.println("ahora con commit");
-System.out.println("ahora no es igual");
 System.out.println( suma.sumador(10, 5) );
 System.out.println("antes pull");
+System.out.println("añadido");
+System.out.println(error);
 	}
 
 }
