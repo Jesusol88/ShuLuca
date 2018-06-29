e8759d0275a2c0699c66acaa12bf92f2a89bc03d
admin <admin@tplwkpcente-a9>
2018-06-29 08:24:03 +0200

    cambio

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -5,10 +5,10 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Holaquetal");
-System.out.println("yo tocando el proyecto");
 System.out.println("ahora con commit");
 System.out.println("nuevo");
 System.out.println( suma.sumador(10, 5) );
+System.out.println("antes pull");
 	}
 
 }
